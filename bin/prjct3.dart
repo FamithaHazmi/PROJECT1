3ca5134920828aff9fab82226827961fe6b71e6b
fami <famifamithahazmi@gmail.com>
2023-05-22 15:56:11 +0530

    commit

---
+++ bin/prjct3.dart
@@ -0,0 +1,29 @@
+class my_class{
+  String  ? name;
+  int ? age;
+  double ? mark;
+  static String course = "flutter";
+}
+void main() {
+  my_class obj = my_class();
+  print("student 1 details");
+  print("my name ${obj.name = "famitha"}");
+  print("my Age ${obj.age = 19}");
+  print("my mark ${obj.mark = 6.5}");
+  print("my course ${my_class.course}");
+
+
+    my_class obj1 = my_class();
+    print("student 2 details");
+    print("my name ${obj1.name = "pammu"}");
+    print("my Age ${obj1.age = 20}");
+    print("my mark ${obj1.mark = 3.5}");
+    print("my course ${my_class.course}");
+
+
+
+
+
+
+
+}
