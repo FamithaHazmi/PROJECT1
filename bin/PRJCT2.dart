140439cae3f5c9f528f75d1e3691921782e085bb
fami <famifamithahazmi@gmail.com>
2023-05-22 15:07:49 +0530

    commit

---
+++ bin/PRJCT2.dart
@@ -0,0 +1,12 @@
+void main(){
+  String name = "famitha";
+  int age=19;
+  double mark=3.5;
+  String course="flutter";
+
+
+  print("my name is $name");
+  print(age);
+  print (course);
+  print(mark);
+}
